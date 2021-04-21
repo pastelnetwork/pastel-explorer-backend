fb0a93c98792af25fc5cbf5e483156db81cd9754
Vatras <fabisiak.tomek@gmail.com>
2021-04-21 09:43:30 +0200

    config: add husky v6 config

--- .huskyrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  hooks: {
-    'pre-commit': 'yarn type-check && lint-staged',
-    'pre-push': 'yarn test && yarn sort:packageJson',
-    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
-  },
-};
