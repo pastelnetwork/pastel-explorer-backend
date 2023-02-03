0bc23020554adb70521da3fbc0c86521527f7552
Tuan Nguyen <titan.ngvtuan@gmail.com>
2023-02-03 11:38:27 +0700

    Improvement for the yarn update-blocks  - Allow start update from xxx block  - Allow start update a single block  - Added “The update-blocks has been stopped at block xx in the last update. Do you want restart the update-blocks from block xxx (y/n)?” question to allow user can restart the update-blocks from the last update.

--- src/scripts/script-update-block.ts
+++ src/scripts/script-update-block.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 
 import { exit } from 'process';
+import prompt from 'prompt';
 import { Connection, createConnection } from 'typeorm';
 
 import { BlockEntity } from '../entity/block.entity';
@@ -9,6 +10,10 @@ import blockService from '../services/block.service';
 import senseRequestsService from '../services/senserequests.service';
 import ticketService from '../services/ticket.service';
 import transactionService from '../services/transaction.service';
+import {
+  readLastBlockHeightFile,
+  writeLastBlockHeightFile,
+} from '../utils/helpers';
 import { batchCreateTransactions } from './seed-blockchain-data/db-utils';
 import { getBlock } from './seed-blockchain-data/get-blocks';
 import {
@@ -20,102 +25,152 @@ import { updateAddressEvents } from './seed-blockchain-data/update-block-data';
 import { BatchAddressEvents } from './seed-blockchain-data/update-database';
 import { updateTickets } from './seed-blockchain-data/updated-ticket';
 
-async function updateBlocks(connection: Connection) {
-  const blockRepo = connection.getRepository(BlockEntity);
-  const processingTimeStart = Date.now();
-  let sqlWhere = null;
-  if (process.argv[2]) {
-    sqlWhere = `CAST(height AS INT) = ${Number(process.argv[2])}`;
-  }
-  const blocksList = await blockRepo
-    .createQueryBuilder()
-    .select(['id', 'height'])
-    .where(sqlWhere)
-    .orderBy('CAST(height AS INT)')
-    .getRawMany();
+const fileName = 'lastUpdateBlockHeight.txt';
 
-  for (let j = 0; j < blocksList.length; j += 1) {
-    const blockHeight = Number(blocksList[j].height);
-    const { block, rawTransactions, vinTransactions } = await getBlock(
-      blockHeight,
-    );
-    if (block?.hash) {
-      console.log(`Processing block ${blockHeight}`);
-      const incorrectBlocks =
-        await blockService.getIncorrectBlocksByHashAndHeight(
-          block.hash,
-          blocksList[j].height,
-        );
-      for (let k = 0; k < incorrectBlocks.length; k++) {
-        await senseRequestsService.deleteTicketByBlockHash(
-          incorrectBlocks[k].id,
-        );
-        const transactions = await transactionService.getAllByBlockHash(
-          incorrectBlocks[k].id,
-        );
-        for (let i = 0; i < transactions.length; i++) {
-          await addressEventsService.deleteEventAndAddressByTransactionHash(
-            transactions[i].id,
-          );
-        }
-        await transactionService.deleteTransactionByBlockHash(
-          incorrectBlocks[k].id,
-        );
-        await blockService.deleteBlockByHash(incorrectBlocks[k].id);
+async function updateBlocks(connection: Connection) {
+  let lastBlockHeight = 0;
+  if (!process.argv[2]) {
+    lastBlockHeight = await readLastBlockHeightFile(fileName);
+  }
+  const updateBlocksData = async (sqlWhere = null) => {
+    const processingTimeStart = Date.now();
+    const blockRepo = connection.getRepository(BlockEntity);
+    const blocksList = await blockRepo
+      .createQueryBuilder()
+      .select(['id', 'height'])
+      .where(sqlWhere)
+      .orderBy('CAST(height AS INT)')
+      .getRawMany();
+
+    for (let j = 0; j < blocksList.length; j += 1) {
+      const blockHeight = Number(blocksList[j].height);
+      if (!process.argv[2]) {
+        await writeLastBlockHeightFile(blockHeight.toString(), fileName);
       }
-      const batchBlock = [block].map(mapBlockFromRPCToJSON);
-      await blockRepo.save(batchBlock);
-      const batchAddressEvents = rawTransactions.reduce<BatchAddressEvents>(
-        (acc, transaction) => [
-          ...acc,
-          ...getAddressEvents(transaction, vinTransactions),
-        ],
-        [],
+      console.log(`Processing block ${blockHeight}`);
+      const { block, rawTransactions, vinTransactions } = await getBlock(
+        blockHeight,
       );
-      const batchTransactions = rawTransactions.map(t =>
-        mapTransactionFromRPCToJSON(t, JSON.stringify(t), batchAddressEvents),
-      );
-      const currentTransactions = await transactionService.getIdByHash(
-        block.hash,
-      );
-      const transactionsLength =
-        currentTransactions.length > batchTransactions.length
-          ? currentTransactions.length
-          : batchTransactions.length;
-      for (let i = 0; i < transactionsLength; i++) {
-        if (batchTransactions[i]?.id) {
-          await batchCreateTransactions(connection, [batchTransactions[i]]);
-          await updateAddressEvents(connection, [
-            {
-              id: batchTransactions[i].id,
-              blockHash: block.hash,
-              height: blockHeight,
-            },
-          ]);
-        }
-        if (currentTransactions[i]?.id) {
-          const transaction = batchTransactions.find(
-            t => t.id === currentTransactions[i]?.id,
+      if (block?.hash) {
+        const incorrectBlocks =
+          await blockService.getIncorrectBlocksByHashAndHeight(
+            block.hash,
+            blocksList[j].height,
           );
-          if (!transaction) {
-            await transactionService.updateBlockHashIsNullByHash(
-              currentTransactions[i].id,
+        for (let k = 0; k < incorrectBlocks.length; k++) {
+          await senseRequestsService.deleteTicketByBlockHash(
+            incorrectBlocks[k].id,
+          );
+          const transactions = await transactionService.getAllByBlockHash(
+            incorrectBlocks[k].id,
+          );
+          for (let i = 0; i < transactions.length; i++) {
+            await addressEventsService.deleteEventAndAddressByTransactionHash(
+              transactions[i].id,
             );
           }
+          await transactionService.deleteTransactionByBlockHash(
+            incorrectBlocks[k].id,
+          );
+          await blockService.deleteBlockByHash(incorrectBlocks[k].id);
         }
+        const batchBlock = [block].map(mapBlockFromRPCToJSON);
+        await blockRepo.save(batchBlock);
+        const batchAddressEvents = rawTransactions.reduce<BatchAddressEvents>(
+          (acc, transaction) => [
+            ...acc,
+            ...getAddressEvents(transaction, vinTransactions),
+          ],
+          [],
+        );
+        const batchTransactions = rawTransactions.map(t =>
+          mapTransactionFromRPCToJSON(t, JSON.stringify(t), batchAddressEvents),
+        );
+        const currentTransactions = await transactionService.getIdByHash(
+          block.hash,
+        );
+        const transactionsLength =
+          currentTransactions.length > batchTransactions.length
+            ? currentTransactions.length
+            : batchTransactions.length;
+        for (let i = 0; i < transactionsLength; i++) {
+          if (batchTransactions[i]?.id) {
+            await batchCreateTransactions(connection, [batchTransactions[i]]);
+            await updateAddressEvents(connection, [
+              {
+                id: batchTransactions[i].id,
+                blockHash: block.hash,
+                height: blockHeight,
+              },
+            ]);
+          }
+          if (currentTransactions[i]?.id) {
+            const transaction = batchTransactions.find(
+              t => t.id === currentTransactions[i]?.id,
+            );
+            if (!transaction) {
+              await transactionService.updateBlockHashIsNullByHash(
+                currentTransactions[i].id,
+              );
+            }
+          }
+        }
+        await blockService.updateTotalTicketsForBlock([], blockHeight);
+        await ticketService.deleteTicketByBlockHeight(blockHeight);
+        await senseRequestsService.deleteTicketByBlockHeight(blockHeight);
+        await updateTickets(connection, block.tx, blockHeight);
       }
-      await blockService.updateTotalTicketsForBlock([], blockHeight);
-      await ticketService.deleteTicketByBlockHeight(blockHeight);
-      await senseRequestsService.deleteTicketByBlockHeight(blockHeight);
-      await updateTickets(connection, block.tx, blockHeight);
     }
+    console.log(
+      `Processing update blocks finished in ${
+        Date.now() - processingTimeStart
+      }ms`,
+    );
+    await writeLastBlockHeightFile('0', fileName);
+    exit();
+  };
+  const promptConfirmMessages = () => {
+    prompt.start();
+
+    prompt.message = '';
+    prompt.delimiter = '';
+    prompt.colors = false;
+
+    prompt.get(
+      {
+        properties: {
+          confirm: {
+            pattern: /^(yes|no|y|n)$/gi,
+            description: `The update-blocks have been stopped at block ${lastBlockHeight} in the last update. Do you want to restart the update-blocks from block ${lastBlockHeight} (y/n)?`,
+            message: 'Type y/n',
+            required: true,
+          },
+        },
+      },
+      async (err, result) => {
+        const c = (result.confirm as string).toLowerCase();
+        if (c != 'y' && c != 'yes') {
+          await updateBlocksData();
+          return;
+        }
+        const sqlWhere = `CAST(height AS INT) >= ${lastBlockHeight}`;
+        await updateBlocksData(sqlWhere);
+      },
+    );
+  };
+  if (!process.argv[2]) {
+    if (lastBlockHeight > 0) {
+      promptConfirmMessages();
+    } else {
+      await updateBlocksData();
+    }
+  } else {
+    let sqlWhere = `CAST(height AS INT) = ${Number(process.argv[2])}`;
+    if (process.argv[2]?.toLowerCase() === 'startat' && process.argv[3]) {
+      sqlWhere = `CAST(height AS INT) >= ${Number(process.argv[3])}`;
+    }
+    await updateBlocksData(sqlWhere);
   }
-  console.log(
-    `Processing update blocks finished in ${
-      Date.now() - processingTimeStart
-    }ms`,
-  );
-  exit();
 }
 
 createConnection().then(updateBlocks);
--- src/services/senserequests.service.ts
+++ src/services/senserequests.service.ts
@@ -27,7 +27,7 @@ class SenseRequestsService {
   async getSenseListByTxId(txid: string) {
     return await this.getRepository()
       .createQueryBuilder()
-      .select('*')
+      .select('imageFileHash, dupeDetectionSystemVersion, transactionHash')
       .where('transactionHash = :txid', { txid })
       .getRawMany();
   }
@@ -35,7 +35,7 @@ class SenseRequestsService {
   async getSenseListByBlockHash(blockHash: string) {
     return await this.getRepository()
       .createQueryBuilder()
-      .select('*')
+      .select('imageFileHash, dupeDetectionSystemVersion, transactionHash')
       .where(
         'transactionHash IN (SELECT id FROM `Transaction` WHERE blockHash = :blockHash)',
         { blockHash },
